5b1282fdb9184823c018b6beee83dd01280ec26d
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-05-17 15:13:57 -0300

    update

--- fsh.c
+++ fsh.c
@@ -1,9 +1,38 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
 
-void process_spawn(char *);
+typedef struct {
+    char *file, *args[10];
+    bool foreground;
+} command;
+
+command command_init();
+
+pid_t process_spawn(command *);
 
 int main() {
+    command commands[2];
+
+    commands[0] = (command){
+        .file = "ls",
+        .args = {"ls", "-la", NULL},
+        .foreground = true,
+    };
+
+    commands[1] = (command){
+        .file = "ps",
+        .args = {"ps", NULL},
+        .foreground = true,
+    };
+
+    process_spawn(commands);
+    process_spawn(commands + 1);
+
+    return 0;
+}
+
+void loop() {
     char *buffer = NULL;
     size_t bufsize = 32;
 
@@ -11,16 +40,15 @@ int main() {
         printf("fsh>");
         getline(&buffer, &bufsize, stdin);
         buffer[2] = '\0';
-        process_spawn("pstree");
+        // process_spawn("pstree");
     }
-
-    return 0;
 }
 
-void process_spawn(char *command) {
+pid_t process_spawn(command *command) {
     pid_t pid = fork();
     if (pid == -1) perror("error");
     if (pid == 0) {
-        if (execlp(command, command, NULL) == -1) perror("error");
+        if (execvp(command->file, command->args) == -1) perror("error");
     }
+    return pid;
 }
