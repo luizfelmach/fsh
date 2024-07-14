ff7f6d4710f62ac415c87a4eac576f0ec2b16fe7
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-07-14 15:46:32 -0300

    update

--- utils.h
+++ utils.h
@@ -1,6 +1,9 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+/*
+ * Macros used for debugging.
+ */
 #define DEBUG_STR(var) printf("DEBUG: %s = '%s'\n", #var, var)
 #define DEBUG_INT(var) printf("DEBUG: %s = '%d'\n", #var, var)
 
